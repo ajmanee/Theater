b6b6d8d6c2faeafe7a4efd1e27a56a7a0e990c21
ajmanee <ajmanee@gmail.com>
2016-08-12 22:12:49 +0400

    simplify the code to only 4 templates

--- client/main.js
+++ client/main.js
@@ -1,11 +1,16 @@
 
 
-
-
-Template.vipTemp.helpers({
+Template.theaterSeatTemp.helpers({
   'seatList': function(){
-    return Seats.find({type: 'vip'});
+    return Seats.find();
   },
+
+
+});
+
+
+Template.seatTemp.helpers({
+
   'selectedClass' : function () {
 
     var seatId = this._id;
@@ -15,59 +20,14 @@ Template.vipTemp.helpers({
     }
 
   }
-
 });
-Template.goldTemp.helpers({
-  'seatList': function(){
-    return Seats.find({type: 'gold'});
-  },
 
-    'selectedClass' : function () {
 
-  var seatId = this._id;
-  var selctedSeat = Session.get('seatId')
-  if (seatId == selctedSeat){
-    return "selected";
-  }
-
-}
-
-});
-Template.silverTemp.helpers({
-  'seatList': function(){
-    return Seats.find({type: 'silver'});
-  },
-  'selectedClass' : function () {
-
-    var seatId = this._id;
-    var selctedSeat = Session.get('seatId')
-    if (seatId == selctedSeat){
-      return "selected";
-    }
-
-  }
-
-});
-Template.normalTemp.helpers({
-  'seatList': function(){
-    return Seats.find({type: 'normal'});
-  },
-  'selectedClass' : function () {
-
-    var seatId = this._id;
-    var selctedSeat = Session.get('seatId')
-    if (seatId == selctedSeat){
-      return "selected";
-    }
-
-  }
-
-});
- Template.seatsTemp.helpers({
+Template.seatsTemp.helpers({
   'seatList': function () {
     var selectedSeatVar = Session.get('seatId');
   }
- });
+});
 
 
 /////// Events ///////
@@ -76,20 +36,23 @@ Template.normalTemp.helpers({
 
 Template.seatsTemp.events({
 
-'click .seatDtls': function(){
-  var seatIdVar = this._id;
-   Session.set('seatId',seatIdVar);
-  var selectedSeatIdOutVar = Session.get('seatId');
-  Seats.update({_id : seatIdVar}, {$set: {lock: true}} );
-  console.log(selectedSeatIdOutVar);
-
-},
-  'submit form': function(){
-     event.preventDefault();
-    var reservedByVar = event.target.nameText.value;
+  'click .seatDtls': function(){
+    var seatIdVar = this._id;
+    Session.set('seatId',seatIdVar);
+    Session.set("visible",true);
     var selectedSeatIdOutVar = Session.get('seatId');
-    Seats.update({_id : selectedSeatIdOutVar}, {$set: {reserverdBy: reservedByVar,lock: false}});
+    Seats.update({_id : seatIdVar}, {$set: {lock: true}} );
   }
 
 });
 
+Template.formTemp.events({
+
+  'submit form': function() {
+    event.preventDefault();
+    var reservedByVar = event.target.nameText.value;
+    var selectedSeatIdOutVar = Session.get('seatId');
+    console.log(selectedSeatIdOutVar)
+    Seats.update({_id: selectedSeatIdOutVar}, {$set: {reserverdBy: reservedByVar, lock: false}});
+  }
+  });
