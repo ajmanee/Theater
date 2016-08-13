865502e8a45a8c318782628158b70a6db26322c7
ajmanee <ajmanee@gmail.com>
2016-08-13 19:04:11 +0400

    change color of all reserved seats and show counter

--- client/main.js
+++ client/main.js
@@ -3,44 +3,74 @@
 Template.theaterSeatTemp.helpers({
   'seatList': function(){
     return Seats.find(); // call all seats from the db
-  },
+  }
 
 
 });
 
 
+
 Template.seatTemp.helpers({
 
+  'reserved' : function() {
+    var isReservedVar = this.reserved;
+    //return console.log(isReservedVar)
+    if (isReservedVar){
+      return "reserved";
+    } else {
+      return "";
+    }
+
+  },
   // this to check wichOne is selected and to be use in the html
+
   'selectedClass' : function () {
 
     var seatId = this._id;
-    var selctedSeat = Session.get('seatId')
+    var selctedSeat = Session.get('seatId');
     if (seatId == selctedSeat){
       return "selected";
     }
 
   }
+
+
 });
 
 
+Template.resCountTemp.helpers({
+
+  // this template to show how many seats are avaiable and reserved
+  'totalSeats' : function () {
+    return Seats.find().count();
+
+  },
+  'reservedSeats' : function () {
+    return Seats.find({reserved: true}).count();
+  }
+})
 
 
 
 /////// Events ///////
 
 
+///////////
 
 Template.seatsTemp.events({
 
   'click .seatDtls': function(){
     var seatIdVar = this._id;
     Session.set('seatId',seatIdVar); // this will save seat id var in the session to be saved other place
+
     var selectedSeatIdOutVar = Session.get('seatId'); // this will get whatever value in the sesion
     Seats.update({_id : seatIdVar}, {$set: {lock: true}} ); // on click action on the db
   }
 
 });
+
+
+
 
 Template.formTemp.events({
 
@@ -48,6 +78,7 @@ Template.formTemp.events({
     event.preventDefault();
     var reservedByVar = event.target.nameText.value; // get the name from the form
     var selectedSeatIdOutVar = Session.get('seatId'); // from the session get whatever value is saved there
-    Seats.update({_id: selectedSeatIdOutVar}, {$set: {reserverdBy: reservedByVar, lock: false}}); // submit db action
+    Seats.update({_id: selectedSeatIdOutVar}, {$set: {reserverdBy: reservedByVar, lock: false, reserved: true}}); // submit db action
   }
   });
+
